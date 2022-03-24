8f2d51f5ac9ee61401c74cb794d295f8c265f0cc
Nayara Gomes <nayara.gomes13@poli.ufrj.br>
2022-03-24 15:39:21 -0300

    inline stylesheets

--- frontend-react/src/components/Modal.js
+++ frontend-react/src/components/Modal.js
@@ -4,7 +4,11 @@ import './Modal.css';
 export default function Modal({children, handleClose}) {
   return ReactDOM.createPortal((
     <div className="modal-backdrop">
-      <div className="modal">
+      <div className="modal" style={{
+        border: "4px solid",
+        borderColor: "#ff4500",
+        textAlign: "center"
+      }}>
         { // O atributo children faz com que o componente identifique o filho que está dentro do componente para que ele seja utilizado
         }
         {children}
