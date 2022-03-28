8094e131bd16681f926da2c42a22920519bf85ca
Nayara Gomes <nayara.gomes13@poli.ufrj.br>
2022-03-28 17:02:51 -0300

    forms & labels in Reactthe onChange Events

--- frontend-react/src/components/NewEventForm.js
+++ frontend-react/src/components/NewEventForm.js
@@ -1,20 +1,29 @@
+import { useState } from 'react';
 import './NewEventForm.css';
 
 export default function NewEventForm() {
-  return (
-    <form className="new-event-form">
-    {/* Esse atributo (htmlFor) é o equivalente ao "for" do HTML, que faz um link de qual input pertence esta label. */}
-      <label>
-        <span>Event Title:</span>
-        <input type="text" /> 
-      </label>
+    const [title, setTitle] = useState('');
+    const [date, setDate] = useState('');
 
-      <label>
-        <span>Event Date:</span>
-        <input type="date" /> 
-      </label>
+    // const handleChange = (e) => {
+    //     setTitle(e.target.value);
+    // }
 
-      <button tpe="submit">Submit</button>
-    </form>
-  )
+    return (
+        <form className="new-event-form">
+            {/* Esse atributo (htmlFor) é o equivalente ao "for" do HTML, que faz um link de qual input pertence esta label. */}
+            <label>
+                <span>Event Title:</span>
+                <input type="text" onChange={(e) => setTitle(e.target.value)} />
+            </label>
+
+            <label>
+                <span>Event Date:</span>
+                <input type="date" onChange={(e) => setDate(e.target.value)} />
+            </label>
+
+            <button tpe="submit">Submit</button>
+            <p>{title} - {date}</p>
+        </form>
+    )
 }
