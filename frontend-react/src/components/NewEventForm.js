f0e06a6ea0cb12d81b451e6a647d2773574f8f35
Nayara Gomes <nayara.gomes13@poli.ufrj.br>
2022-03-29 10:03:45 -0300

    controlled inputs

--- frontend-react/src/components/NewEventForm.js
+++ frontend-react/src/components/NewEventForm.js
@@ -9,21 +9,35 @@ export default function NewEventForm() {
     //     setTitle(e.target.value);
     // }
 
+    const resetForm = () => {
+        setTitle('');
+        setDate('');
+    }
+
     return (
         <form className="new-event-form">
             {/* Esse atributo (htmlFor) é o equivalente ao "for" do HTML, que faz um link de qual input pertence esta label. */}
             <label>
                 <span>Event Title:</span>
-                <input type="text" onChange={(e) => setTitle(e.target.value)} />
+                <input 
+                    type="text"
+                    onChange={(e) => setTitle(e.target.value)}
+                    value={title}
+                />
             </label>
 
             <label>
                 <span>Event Date:</span>
-                <input type="date" onChange={(e) => setDate(e.target.value)} />
+                <input
+                    type="date"
+                    onChange={(e) => setDate(e.target.value)}
+                    value={date}
+                />
             </label>
 
             <button tpe="submit">Submit</button>
             <p>{title} - {date}</p>
+            <p onClick={resetForm}>Reset form</p>
         </form>
     )
 }
