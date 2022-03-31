7ea5d278d1b078d1e91e8f06c706cb93459b4088
Nayara Gomes <nayara.gomes13@poli.ufrj.br>
2022-03-30 23:09:35 -0300

    useCallback for Function Dependencies

--- tripago/src/components/TripList.js
+++ tripago/src/components/TripList.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 
 //styles
 import './TripList.css';
@@ -7,15 +7,20 @@ export default function TripList() {
     const [trips, setTrips] = useState([]);
     const [url, setUrl] = useState('http://localhost:3000/trips');
 
-    console.log(trips);
+    const fetchTrips = useCallback(async () => {
+        const response = await fetch(url);
+        const json = await response.json();
+        setTrips(json);
+    })
 
     // Com o useEffect, a cada reload do componente, a função do primeiro argumento só rodará se algum dos seus dependentes (segundo argumento) for modificado
     // Se no segundo argumento estiver vazio ([]), a função só é executada quando a página é carregada
     useEffect(() => {
-        fetch(url)
-            .then(response => response.json())
-            .then(json => setTrips(json));
-    }, [url])
+        fetchTrips();
+    }, [fetchTrips])
+
+    // Quando o React compara duas funções para saber se mudou ou não, na verdade ele usa como referência de comparação lugares na memória
+    // Mas a cada reavaliação, a função ocupa lugares diferentes na memória, então ele nunca vai achar um resultado que bata.
 
     return (
         <div className="trip-list">
