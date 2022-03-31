f41430d2e7215d64552923cfa309c8ca6c464e21
Nayara Gomes <nayara.gomes13@poli.ufrj.br>
2022-03-30 21:15:05 -0300

    Fetching Data with useEffect

--- tripago/src/components/TripList.js
+++ tripago/src/components/TripList.js
@@ -1,21 +1,32 @@
-import React, { useState } from 'react'
+import { useState, useEffect } from 'react';
+
+//styles
+import './TripList.css';
 
 export default function TripList() {
-    const [trips, setTrips] = useState()
-    // Esse código rodará infinitamente pois o React trabalha de forma a recarregar o componente inteiro toda vez que alguma informação é atualizada,
-    // então o que acontece é que, ao atualizar o trips (linha 13), o componente é recarregado e, por consequência, o método fetch é rodado novamente
-    // e com isso, ele faz uma nova chamada na API, obtendo a resposta e chamando o setTrips de novo, o que faz com que isso fique em loop infinito
+    const [trips, setTrips] = useState([])
+
     console.log(trips);
-    // É feita uma chamada API para captar a responsta desse endpoint
-    fetch('http://localhost:3000/trips')
-    // Após termos a resposta, essa função pega o objeto da resposta e usa o método json para ter acesso ao dado 
-        .then( response => response.json())
-    // Quando esse dado json é retornado na função acima, nós podemos pegá-lo e fazer o que quisermos
-        .then(json => setTrips(json));
+
+    // Com o useEffect, a cada reload do componente, a função do primeiro argumento só rodará se algum dos seus dependentes (segundo argumento) for modificado
+    // Se no segundo argumento estiver vazio ([]), a função só é executada quando a página é carregada
+    useEffect(() => {
+        fetch('http://localhost:3000/trips')
+            .then(response => response.json())
+            .then(json => setTrips(json));
+    }, [])
 
     return (
-        <div>
+        <div className="trip-list">
             <h2>Trip List</h2>
+            <ul>
+                {trips.map(trip => (
+                    <li key={trip.id}>
+                        <h3>{trip.title}</h3>
+                        <p>{trip.price}</p>
+                    </li>
+                ))}
+            </ul>
         </div>
     )
 }
