20bd7a2d691304961a77989f64cc0e6fcba7574b
Nayara Gomes <nayara.gomes13@poli.ufrj.br>
2022-03-31 19:24:35 -0300

    Adding a loading/pending state

--- tripago/src/hooks/useFetch.js
+++ tripago/src/hooks/useFetch.js
@@ -3,13 +3,17 @@ import { useState, useEffect } from "react";
 // O bom de fazer hooks customizadas é que você consegue reutilizá-las em outros componentes
 export const useFetch = (url) => {
     const [data, setData] = useState(null);
+    const [ isPending, setIsPending ] = useState(false);
+
     useEffect(() => {
         const fetchData = async () => {
+            setIsPending(true);
             const res = await fetch(url);
             console.log(url);
 
             const data = await res.json();
 
+            setIsPending(false);
             setData(data)
         }
 
@@ -18,5 +22,5 @@ export const useFetch = (url) => {
 
 
     //sempre que você tem um hook personalizado, terá retorno na função, geralmente um array de informações
-    return { data }
+    return { data, isPending }
 }
