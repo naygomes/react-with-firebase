658e45759b88319961587ef5f00895f8998dcf92
Nayara Gomes <nayara.gomes13@poli.ufrj.br>
2022-03-31 19:40:36 -0300

    handling errors

--- tripago/src/hooks/useFetch.js
+++ tripago/src/hooks/useFetch.js
@@ -3,18 +3,30 @@ import { useState, useEffect } from "react";
 // O bom de fazer hooks customizadas é que você consegue reutilizá-las em outros componentes
 export const useFetch = (url) => {
     const [data, setData] = useState(null);
-    const [ isPending, setIsPending ] = useState(false);
-
+    const [isPending, setIsPending] = useState(false);
+    const [error, setError] = useState(null);
     useEffect(() => {
         const fetchData = async () => {
             setIsPending(true);
-            const res = await fetch(url);
-            console.log(url);
 
-            const data = await res.json();
+            try {
+                const res = await fetch(url);
+                const data = await res.json();
 
-            setIsPending(false);
-            setData(data)
+                if (!res.ok) {
+                    throw new Error(res.statusText);
+                }
+
+                setIsPending(false);
+                setData(data)
+                setError(null);
+            } catch (error) {
+                setIsPending(false);
+
+                setError('Could not fetch the data');
+                console.log(error.message);
+            }
+
         }
 
         fetchData()
@@ -22,5 +34,5 @@ export const useFetch = (url) => {
 
 
     //sempre que você tem um hook personalizado, terá retorno na função, geralmente um array de informações
-    return { data, isPending }
+    return { data, isPending, error }
 }
