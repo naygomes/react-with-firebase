aada89a0b2438e636368fd9e764a5931717a9db6
Nayara Gomes <nayara.gomes13@poli.ufrj.br>
2022-04-26 10:52:41 -0300

    making a navbar component

--- cooking-ninja/src/App.js
+++ cooking-ninja/src/App.js
@@ -5,6 +5,9 @@ import Create from './pages/create/Create';
 import Recipe from './pages/recipe/Recipe';
 import Search from './pages/search/Search';
 
+// components
+import Navbar from './components/Navbar';
+
 // styles
 import './App.css';
 
@@ -12,6 +15,7 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Navbar />
         <Switch>
           <Route exact path='/'>
             <Home />
