ec1a08433203de80959ee341153277d8b47c976e
Nayara Gomes <nayara.gomes13@poli.ufrj.br>
2022-04-26 15:07:09 -0300

    challenge - fetching a single recipe

--- cooking-ninja/src/pages/recipe/Recipe.js
+++ cooking-ninja/src/pages/recipe/Recipe.js
@@ -1,9 +1,26 @@
+import { useParams } from "react-router-dom";
+import { useFetch } from "../../hooks/useFetch";
+
+// styles
 import './Recipe.css';
 
 export default function Recipe() {
+  const { id } = useParams();
+  const url = 'http://localhost:3000/recipes/' + id;
+  const { data: recipe, isPending, error } = useFetch(url);
+
   return (
-    <div>
-      Recipe
+    <div className="recipe">
+      {isPending && <p className="loading">Loading...</p>}
+      {error && <p className="error">{error}</p>}
+      {recipe && (
+        <div >
+          <h2>{recipe.title}</h2>
+          <p>By {recipe.cookingTime}</p>
+          <p>{recipe.ingredients}</p>
+          <p>{recipe.method}</p>
+        </div>
+      )}
     </div>
   )
 }
