4b5c4f4fb0cd56ae9b412a852feafa6df346cbb1
Nayara Gomes <nayara.gomes13@poli.ufrj.br>
2022-04-26 00:13:48 -0300

    programmatic redirect

--- multipage-site/src/pages/Article.js
+++ multipage-site/src/pages/Article.js
@@ -1,10 +1,19 @@
-import { useParams } from "react-router-dom";
+import { useEffect } from "react";
+import { useParams, useHistory } from "react-router-dom";
 import { useFetch } from "../hooks/useFetch";
 
 export default function Article() {
     const { id } = useParams();
     const url = 'http://localhost:3000/articles/' + id;
     const { data: article, isPending, error } = useFetch(url);
+    const history = useHistory();
+
+    useEffect(() => {
+        if (error) {
+            // Para retornar para a página anterior de acordo com o histórico --> history.goBack();
+            setTimeout(() => {history.push('')}, 2000);
+        }
+    }, [error, history])
 
     return (
         <div>
