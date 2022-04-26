65eb05927a67f740efed0094ece7f9215404d452
Nayara Gomes <nayara.gomes13@poli.ufrj.br>
2022-04-26 10:38:05 -0300

    Router & Pages Setup

--- cooking-ninja/src/App.js
+++ cooking-ninja/src/App.js
@@ -1,9 +1,32 @@
-import './App.css'
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Home from './pages/home/Home';
+import Create from './pages/create/Create';
+import Recipe from './pages/recipe/Recipe';
+import Search from './pages/search/Search';
+
+// styles
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+          <Route path='/create'>
+            <Create />
+          </Route>
+          <Route path='/recipes/:id'>
+            <Recipe />
+          </Route>
+          <Route path='/search'>
+            <Search />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
