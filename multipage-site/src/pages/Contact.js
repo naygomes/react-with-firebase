cfa12b74ce759c2110577acfd6eb0e80d43c0b80
Nayara Gomes <nayara.gomes13@poli.ufrj.br>
2022-04-26 01:06:42 -0300

    query parameters

--- multipage-site/src/pages/Contact.js
+++ multipage-site/src/pages/Contact.js
@@ -1,7 +1,15 @@
+import { useLocation } from "react-router-dom";
+
 export default function Contact() {
+    const queryString = useLocation().search;
+    console.log(queryString);
+
+    const queryParams = new URLSearchParams(queryString);
+    const name = queryParams.get("name");
+
     return (
         <div>
-            <h2>Contact</h2>
+            <h2> Hey { name }, contact us here... </h2>
             <p>Culpa nulla qui irure tempor commodo laborum magna Lorem pariatur nulla. 
                 Nulla cupidatat fugiat esse est laboris veniam ut labore aute culpa nostrud ut dolor sunt.
                 Pariatur eiusmod reprehenderit reprehenderit magna culpa duis et non.
