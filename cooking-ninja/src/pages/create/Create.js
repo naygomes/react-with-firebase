6ac8f273d583c974c2f19f56c268a6f24cefcd3f
Nayara Gomes <nayara.gomes13@poli.ufrj.br>
2022-04-26 16:50:53 -0300

    adding multiples ingredients

--- cooking-ninja/src/pages/create/Create.js
+++ cooking-ninja/src/pages/create/Create.js
@@ -1,14 +1,29 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import './Create.css';
 
 export default function Create() {
   const [title, setTitle] = useState('');
   const [method, setMethod] = useState('');
   const [cookingTime, setCookingTime] = useState('');
+  const [ingredients, setIngredients] = useState([]);
+  const [newIngredient, setNewIngredient] = useState('');
+  const ingredientInput = useRef(null);
+
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(title, method, cookingTime)
+    console.log(title, method, cookingTime, ingredients)
+  }
+
+  const handleAdd = (e) => {
+    e.preventDefault()
+    const ing = newIngredient.trim()
+
+    if (ing && !ingredients.includes(ing)) {
+      setIngredients(prevIngredients => [...prevIngredients, newIngredient])
+    }
+    setNewIngredient('')
+    ingredientInput.current.focus()
   }
 
   return (
@@ -25,6 +40,20 @@ export default function Create() {
             required
           />
         </label>
+
+        <label>
+          <span>Recipe Ingredients:</span>
+          <div className='ingredients'>
+            <input
+              type="text"
+              onChange={(e) => setNewIngredient(e.target.value)}
+              value={newIngredient}
+              ref={ingredientInput}
+            />
+            <button className="btn" onClick={handleAdd}>Add</button>
+          </div>
+        </label>
+        <p>Current ingredients: {ingredients.map(i => <em key={i}>{i}, </em>)}</p>
 
         <label>
           <span>Recipe Method:</span>
